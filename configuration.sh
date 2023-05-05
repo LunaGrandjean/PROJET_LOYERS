a52331adf591c03feea9d4772cb84b836fbadadb
BagotAlexis <125754514+BagotAlexis@users.noreply.github.com>
2023-05-05 15:08:34 +0200

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,11 +1,13 @@
-# Créer un nouvel environnement virtuel Conda
+#!/bin/bash
+
+# Créez un nouvel environnement virtuel Conda
 conda create -n myenv python=3.8
 
-# Activer l'environnement virtuel
+# Activez l'environnement virtuel
 conda activate myenv
 
-# Installer les bibliothèques nécessaires
-conda install pandas=2.0.1 ydata_profiling=4.1.2 glob=3.11.3
+# Installez les bibliothèques nécessaires
+conda install pandas matplotlib seaborn jupyter chardet pandas_profiling glob
 
-# Afficher un message indiquant que l'installation est terminée
+# Affichez un message indiquant que l'installation est terminée
 echo "L'environnement virtuel a été créé et les bibliothèques ont été installées."
